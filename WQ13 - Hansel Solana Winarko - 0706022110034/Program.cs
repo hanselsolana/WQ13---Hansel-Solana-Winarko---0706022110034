88b3bb47e5b63eb0a657724e022f2f140368b36a
asus <asus@LAPTOP-U90DK29T>
2021-12-06 09:12:26 +0700

    Add project files.

---
+++ WQ13 - Hansel Solana Winarko - 0706022110034/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WQ13___Hansel_Solana_Winarko___0706022110034
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.Write("Berapa data = ");
+			int data = Convert.ToInt16(Console.ReadLine());
+			Console.Write("data ke-1 \nNIM : ");
+			int nim = Convert.ToInt16(Console.ReadLine());
+			Console.Write("NAMA : ");
+			string nama = Console.ReadLine();
+			Console.Write("JENIS KELAMIN : ");
+			string jenisKelamin = Console.ReadLine();
+			Console.Write("TAHUN MASUK : ");
+			string tahunMasuk = Console.ReadLine();
+			Console.Write("PROGRAM STUDI : ");
+			int programStudi = Convert.ToInt16(Console.ReadLine());
+			Console.Write("KELAS : ");
+			int kelas = Convert.ToInt16(Console.ReadLine());
+			Console.Write("PRINT HASIL ? (y/n)");
+			int printHasil = Convert.ToInt16(Console.ReadLine());
+
+		}
+    }
+}
